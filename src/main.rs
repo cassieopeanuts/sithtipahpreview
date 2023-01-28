84594101b2d6210e1d53145aca29f9c811907b21
cassieopeanuts <92317800+cassieopeanuts@users.noreply.github.com>
2023-01-28 19:53:43 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,45 @@
+use modules::commands::{tip, update, balance, register};
 use serenity::{
-    model::{event::ResumedEvent, gateway::Ready, gateway::GatewayIntents },
+    model::{gateway::Ready, gateway::GatewayIntents },
     prelude::*,
 };
-
+use serenity::model::channel::Message;
 use serenity::client::{Client, Context,};
 use serenity::framework::standard::{
-    StandardFramework,
+    StandardFramework, macros::{command, group}, CommandResult,
 };
-
 
 use async_trait::*;
 use tokio;
 mod modules;
-use std::*;
+use std::{*, path::Prefix};
 use dotenv::dotenv;
 
 use crate::modules::ALLCOMMS_GROUP;
 
+
 struct Handler;
+
 #[async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, _ctx: Context, ready: Ready) {
+    async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
 
-    
+    async fn message(&self, ctx: Context, msg: Message) {
 
-    async fn resume(&self, _ctx: Context, _: ResumedEvent) {
-        println!("Resumed");
+        let _framework = StandardFramework::new()
+            .configure(|c| c
+                .prefix("!")
+                .allow_dm(false)
+                .with_whitespace(true)
+                .delimiters(vec![",", ";"])
+                .case_insensitivity(true)
+            )
+            .group(&ALLCOMMS_GROUP);
+        }
     }
-}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -52,13 +62,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .group(&ALLCOMMS_GROUP);
 
+    let handler = Handler;
+
     let mut client = Client::builder(token, GatewayIntents::empty())
         
         .framework(framework)
-        .event_handler(Handler)
+        .event_handler(handler)
         .application_id(application_id)
         .await
         .expect("Error creating client");
+
 
     // Login to Discord and start the event loop
     if let Err(why) = client.start().await {
